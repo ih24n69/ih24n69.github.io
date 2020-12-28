595d1c2709ae5eceb33ebfe844ae98bcb7a6dc0c
Muhammad Ihsan <muhammadihsan69@gmail.com>
2020-12-28 19:54:00 +0700

    wedding:make proper effect music button

--- iawedding/js/music.js
+++ iawedding/js/music.js
@@ -1,18 +1,16 @@
 const button = document.querySelector("#button");
-const icon = document.querySelector("#button > img");
 const audio = document.querySelector("audio");
 
 button.addEventListener("click", () => {
   if (audio.paused) {
     audio.volume = 0.2;
     audio.play();
-    icon.classList.remove('fa-volume-up');
-    icon.classList.add('fa-volume-mute');
+	button.classList.remove("on");
+	button.classList.add("off");
     
   } else {
     audio.pause();
-   icon.classList.remove('fa-volume-mute');
-   icon.classList.add('fa-volume-up');
+   button.classList.remove("off");
+   button.classList.add("on");
   }
-  button.classList.add("fade");
 });
